26d187dd843eb09de05d7d7e8eefabe39582b4a8
Trikcode <ayesiganobert@gmail.com>
2022-04-16 08:25:55 +0300

    exec

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Lockboxes
-Contains method that finds the keys to open other lockboxes
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
 """
 
 
 def canUnlockAll(boxes):
-    """
-    Function that determines if you can open all the lockboxes
-    Args:
-        boxes: list of lists of integers
-    Returns:
-        True if you can open all the lockboxes, False otherwise
-    """
-    unlocked_boxes = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked_boxes and key != box_id:
-                unlocked_boxes.append(key)
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
 
-    if len(unlocked_boxes) == len(boxes):
-        return True
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
     return False
