267069dbd933ce63cf4cdf811f2795177d9fbc6c
Trikcode <ayesiganobert@gmail.com>
2022-05-03 16:57:34 +0300

    change bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
+"""UTF-8 validator"""
 
 
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+        Check that a sequence of byte values follows the UTF-8 encoding
+        rules.  Does not check for canonicalization (i.e. overlong encodings
+        are acceptable).
+        """
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
+    data = iter(data)
+    for leading_byte in data:
+        leading_ones = _count_leading_ones(leading_byte)
+        if leading_ones in [1, 7, 8]:
             return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
-
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
+        for _ in range(leading_ones - 1):
+            trailing_byte = next(data, None)
+            if trailing_byte is None or trailing_byte >> 6 != 0b10:
                 return False
     return True
+
+
+def _count_leading_ones(byte):
+    """Counts the leading ones."""
+
+    for i in range(8):
+        if byte >> 7 - i == 0b11111111 >> 7 - i & ~1:
+            return i
+    return 8
